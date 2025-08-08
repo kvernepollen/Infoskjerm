bf35ec8020abcf4c9be0eaf13b8744b8788346d6
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-08-08 19:27:50 +0200

    Upgrade - one page per slide:
    * Fix bug by loading a full new page for each slide.
    * Add progress bar at bottom
    * Add Possibility to pause and resume with spacebar.

---
+++ load-next.js
@@ -0,0 +1,46 @@
+// Config
+const VENTETID = 10_000; // ms
+const nextMap = {
+  "index.html": "index2.html",
+  "index2.html": "index3.html",
+  "index3.html": "index4.html",
+  "index4.html": "index.html",
+};
+
+// Get element
+const progressBar = document.getElementById("progress-bar");
+
+
+// Timeout handler
+const timeoutHandle = setTimeout(() => {
+  const progressState = progressBar.getAttribute("data-state");
+
+  if (progressState === "paused") {
+    return;
+  }
+  toNextSlide();
+}, VENTETID);
+
+
+// Key handler
+document.addEventListener("keydown", e => {
+  if (e.key == " ") {
+    let progressState = progressBar.getAttribute("data-state");
+
+    if (progressState === "running" || progressState === "") {
+      progressState = "paused";
+    } else {
+      progressState = "running";
+      toNextSlide();
+    }
+    progressBar.setAttribute("data-state", progressState);
+  }
+})
+
+// to next slide function
+function toNextSlide() {
+  const params = new URLSearchParams(window.location.search);
+  const next = params.get("next") ?? nextMap["index.html"];
+  
+  window.location.href = `${next}?next=${nextMap[next]}`;
+}
